cdbd682bdc016cf22b134f390a3b620e193032cf
Oliver Steele <steele@osteele.com>
2018-06-28 15:24:54 +0700

    Display images as Semantic UI cards

--- front/src/data/actions.js
+++ front/src/data/actions.js
@@ -4,10 +4,20 @@ export const ActionTypes = {
     SET_SORT_ORDER: 'SET_SORT_ORDER'
 };
 
+const decodeEvent = event =>
+    _.merge({}, event, {
+        timestamp: Date.parse(event.timestamp.replace(/\..+/, ''))
+    });
+
 export const getEvents = () => (dispatch, getStore) =>
     getStore()
         .apiClient.get('/images/')
-        .then(({ data }) => dispatch({ type: ActionTypes.SET_IMAGES, data }));
+        .then(({ data }) =>
+            dispatch({
+                type: ActionTypes.SET_IMAGES,
+                data: data.map(decodeEvent)
+            })
+        );
 
 export const setImageSize = imageSize => dispatch =>
     dispatch({ type: ActionTypes.SET_IMAGE_SIZE, imageSize });
