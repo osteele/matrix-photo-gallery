31586b29d24fb4c54b689c1bc3b8904d8e8e587d
Oliver Steele <steele@osteele.com>
2018-06-28 14:09:58 +0700

    Add front end

---
+++ front/src/data/store.js
@@ -0,0 +1,32 @@
+import { routerMiddleware, routerReducer } from 'react-router-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import * as reducers from './reducers';
+
+const debug = true;
+
+export default function setupStore(apiClient) {
+    const initialState = {
+        apiClient,
+        images: []
+    };
+
+    const middleware =
+        debug && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+            ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
+                  applyMiddleware(
+                      thunkMiddleware, // lets us dispatch() functions
+                      routerMiddleware(history)
+                  )
+              )
+            : applyMiddleware(
+                  thunkMiddleware, // lets us dispatch() functions
+                  routerMiddleware(history)
+              );
+
+    return createStore(
+        combineReducers({ ...reducers, router: routerReducer }),
+        initialState,
+        middleware
+    );
+}
