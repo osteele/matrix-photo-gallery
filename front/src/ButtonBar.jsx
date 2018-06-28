b7366ad17c637d3e9b8eb9a8d0662497611bf67d
Oliver Steele <steele@osteele.com>
2018-06-28 15:04:41 +0700

    Move components to own files

---
+++ front/src/ButtonBar.jsx
@@ -0,0 +1,58 @@
+import { connect } from 'react-redux';
+import { setImageSize, setSortOrder } from './data/actions';
+
+const ButtonBar = ({ setImageSize, setSortOrder }) => (
+    <div>
+        <div className="ui icon buttons">
+            <button className="ui button" onClick={() => setImageSize('tiny')}>
+                <i className="small square icon" />
+            </button>
+            <button className="ui button" onClick={() => setImageSize('small')}>
+                <i className="medium square icon" />
+            </button>
+            <button
+                className="ui button"
+                onClick={() => setImageSize('medium')}
+            >
+                <i className="large square icon" />
+            </button>
+        </div>
+        <div className="ui icon buttons">
+            <button
+                className="ui button"
+                onClick={() => setSortOrder('date-asc')}
+            >
+                <i className="sort numeric up icon" />
+            </button>
+            <button
+                className="ui button"
+                onClick={() => setSortOrder('date-desc')}
+            >
+                <i className="sort numeric down icon" />
+            </button>
+            <button
+                className="ui button"
+                onClick={() => setSortOrder('sender')}
+            >
+                <i className="sort alphabet up icon" />
+            </button>
+        </div>
+    </div>
+);
+
+const mapStateToProps = state => ({
+    imageSize: state.imageSize,
+    sortOrder: state.sortOrder
+});
+
+const mapDispatchToProps = dispatch => ({
+    setImageSize: size => dispatch(setImageSize(size)),
+    setSortOrder: sortOrder => dispatch(setSortOrder(sortOrder))
+});
+
+const ButtonBarContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ButtonBar);
+
+export default ButtonBarContainer;
