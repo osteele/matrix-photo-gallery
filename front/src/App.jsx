30a37d7177e20b115c4e1e5cad395a8491139d4e
Oliver Steele <steele@osteele.com>
2018-06-29 08:05:49 +0700

    Add a footer, HTML title

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 import ButtonBar from './ButtonBar';
 import Clock from './Clock';
 import ImageGrid from './ImageGrid';
+import './site.scss';
 
 const Gallery = () => (
     <div>
@@ -10,19 +11,40 @@ const Gallery = () => (
     </div>
 );
 
+const Footer = () => (
+    <footer className="ui vertical footer segment">
+        <div className="ui center aligned container">
+            View on{' '}
+            <a href="https://github.com/osteele/matrix-photo-gallery">GitHub</a>
+        </div>
+    </footer>
+);
+
 const App = () => (
     <Router>
-        <div className="ui container">
-            <nav className="ui tabular menu">
-                <NavLink className="item" exact activeClassName="active" to="/">
-                    Gallery
-                </NavLink>
-                <NavLink className="item" activeClassName="active" to="/clock">
-                    Clock
-                </NavLink>
-            </nav>
-            <Route exact path="/" component={Gallery} />
-            <Route path="/clock" component={Clock} />
+        <div className="app">
+            <main className="ui container">
+                <nav className="ui tabular menu">
+                    <NavLink
+                        className="item"
+                        exact
+                        activeClassName="active"
+                        to="/"
+                    >
+                        Gallery
+                    </NavLink>
+                    <NavLink
+                        className="item"
+                        activeClassName="active"
+                        to="/clock"
+                    >
+                        Clock
+                    </NavLink>
+                </nav>
+                <Route exact path="/" component={Gallery} />
+                <Route path="/clock" component={Clock} />
+            </main>
+            <Footer />
         </div>
     </Router>
 );
