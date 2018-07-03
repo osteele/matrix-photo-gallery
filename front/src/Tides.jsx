2a6b9c0022b964ca7d712ebe89048c289d88132c
Oliver Steele <steele@osteele.com>
2018-07-03 10:47:21 +0700

    Smaller images; better factored

--- front/src/Tides.jsx
+++ front/src/Tides.jsx
@@ -4,6 +4,7 @@ import withBackground from './background';
 import { setBackground } from './data/actions';
 
 const Tides = ({ images, heartbeat }) => {
+    images = images.filter(image => image.tideLevel !== undefined);
     const width = 800;
     const height = 500;
     // tide levels
@@ -12,9 +13,11 @@ const Tides = ({ images, heartbeat }) => {
     const tideMax = levels.max().value();
     const tideRange = tideMax - tideMin;
     // current tide level
-    const period = 30 * 1000;
+    const tidePeriod = 30 * 1000; // complete tide cycle in ms
     const tideLevel =
-        tideMin + tideRange * Math.cos((heartbeat * 2 * Math.PI) / period);
+        tideMin + tideRange * Math.cos((heartbeat * 2 * Math.PI) / tidePeriod);
+    // current water level
+    const waterLevel = tideLevel;
     return (
         <section
             className="tides"
@@ -25,31 +28,33 @@ const Tides = ({ images, heartbeat }) => {
             }}
         >
             <h2>Tides</h2>
-            {images.map((image, i) => {
-                const x = (i * width) / images.length;
-                const y = ((image.tideLevel - tideMin) / tideRange) * height;
-                const alpha =
-                    1 - (image.tideLevel - tideLevel) ** 2 / tideRange ** 2;
-                return (
-                    image.tideLevel && (
-                        <img
-                            className="ui small circular image"
-                            key={image.event_id}
-                            style={{
-                                position: 'absolute',
-                                left: x + 'px',
-                                top: y + 'px',
-                                display: alpha > 0 ? 'block' : 'none',
-                                opacity: Math.max(0, alpha)
-                            }}
-                            src={image.thumbnail_url}
-                        />
-                    )
-                );
-            })}
+            {images.map((image, i) => (
+                <Image
+                    image={image}
+                    key={image.event_id}
+                    x={(i * width) / images.length}
+                    y={((image.tideLevel - tideMin) / tideRange) * height}
+                    alpha={
+                        1 - (image.tideLevel - waterLevel) ** 2 / tideRange ** 2
+                    }
+                />
+            ))}
         </section>
     );
 };
+
+const Image = ({ image, x, y, alpha }) => (
+    <img
+        className="ui circular image"
+        style={{
+            left: x + 'px',
+            top: y + 'px',
+            display: alpha > 0 ? 'block' : 'none',
+            opacity: Math.max(0, alpha)
+        }}
+        src={image.thumbnail_url}
+    />
+);
 
 const mapStateToProps = state => ({
     heartbeat: state.heartbeat,
