a36a5777f0ef7ff726854d1d1d840dd37e920f46
Oliver Steele <steele@osteele.com>
2018-07-07 10:53:43 +0700

    Factor Gallery into own file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,8 @@
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
-import ButtonBar from './ButtonBar';
 import Clock from './Clock';
-import ImageGrid from './ImageGrid';
+import Gallery from './Gallery';
 import './site.scss';
-
-const Gallery = () => (
-    <div>
-        <ButtonBar />
-        <ImageGrid />
-    </div>
-);
 
 const Footer = () => (
     <footer className="ui vertical footer segment">
